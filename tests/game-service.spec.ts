c75f1481fa1a105bf7d2fd1b5f831adb976561a8
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-06-25 11:23:15 +0200

    Added test for checking fields content

--- tests/game-service.spec.ts
+++ tests/game-service.spec.ts
@@ -2,7 +2,7 @@ import { LeaderboardsStorage } from './../src/leaderboards/leaderboards-storage'
 import { GameFieldFactory } from './../src/game/game-field-factory';
 import { SessionStorage } from './../src/game/session-storage';
 import { GameService } from './../src/game/game-service';
-
+import { GameField } from '../src/game/game-field';
 
 describe('GameService', () => {
     let gameService: GameService;
@@ -26,10 +26,43 @@ describe('GameService', () => {
 
         expect(gameFieldFactory.createGameField).toHaveBeenCalled();
         expect(sessionStorage.putValue).toHaveBeenCalledWith(testName, {
-            exploredCellsCount: 0,
+            turnsTaken: 0,
             foundTreasuresCount: 0,
             gameField: 'Some game field'
         });
         expect(sessionId).toEqual(testName);
+    });
+
+    it('should perform a field check', () => {
+        const sessionId = 'test';
+        const pointsToReveal = [{x: 1, y: 1}, {x: 2, y: 3}, {x: 4, y: 2}];
+
+        const gameFieldMock = new GameField();
+        spyOn(gameFieldMock, 'getCellContent').and.callFake((x: number, y: number) => {
+            if (x === 1 && y === 1) {
+                return 'T'
+            };
+
+            if (x === 2 && y === 3) {
+                return '2'
+            };
+
+            if (x === 4 && y === 2) {
+                return '1';
+            }
+        });
+        spyOn(sessionStorage, 'getValue').and.returnValue({
+            turnsTaken: 0,
+            foundTreasuresCount: 0,
+            gameField: gameFieldMock,
+        });
+
+        const result = gameService.checkFields(sessionId, pointsToReveal);
+
+        expect(result).toEqual([
+            {x: 1, y: 1, content: 'T'},
+            {x: 2, y: 3, content: '2'},
+            {x: 4, y: 2, content: '1'}
+        ]);
     });
 });
--- tests/index.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Sample test', () => {
-    
-
-    it('should work', () => {
-        expect(2 + 2).toEqual(4);
-    });
-});
